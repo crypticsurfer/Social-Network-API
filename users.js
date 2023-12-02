0b1a86486b62065a50b27f6adaa1f12d1d827ca5
crypticsurfer <cb.connor.bishop@gmail.com>
2023-12-01 21:11:16 -0700

    added put and updated

--- users.js
+++ users.js
@@ -42,8 +42,35 @@ const getAllUsers = async (req, res, next) => {
     }
   }
 
+  const updateUser = async (req, res, next) => {
+    try {
+      const User = db.model('User');
+      const filter = { _id: req.params._id };
+      const instance = await User.findOne(filter).exec();
+  
+      if (!instance) {
+        res.status(404).json({ message: 'No user with this id!' });
+        return;
+      }
+      
+      if (req.body.username) {
+        instance.username = req.body.username;
+      }
+      if (req.body.email) {
+        instance.email = req.body.email;
+      }
+      await instance.save();
+      console.log("Updated user!");
+      res.send(instance);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+  
 router.get('/', getAllUsers);
 router.get('/:_id', getUser);
 router.post('/', addUser);
+
+router.put('/:_id', updateUser);
 
 module.exports = router;
